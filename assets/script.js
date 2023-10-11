a559e2b00b55565cc8a975f8668e3c51bbf54f04
Kaveri Chougule <chougulekaveri123@gmail.com>
2023-10-11 20:10:56 +0530

    Initial Commit

---
+++ assets/script.js
@@ -0,0 +1,49 @@
+let tip_percent=document.querySelectorAll('.tips')
+tip_percent.forEach(function(val){
+    val.addEventListener('click',handleTipClick)
+})
+
+let tipValue=0.05;
+function handleTipClick(event){
+    tip_percent.forEach(function(val){
+        val.classList.remove("active-tip")
+        if(event.target.innerHTML==val.innerHTML){
+            val.classList.add("active-tip")
+            tipValue=parseFloat(val.innerHTML)
+        }
+    })
+    // console.log(tipValue)
+}
+
+
+
+document.addEventListener('click',calculate_tip)
+function calculate_tip(event){
+    let amount=parseFloat(document.getElementById('bill').value)
+    let people=document.querySelector('.people-input').value
+    let error=document.getElementsByClassName("error")
+    if(people<1){
+        error[0].style.display="block"
+    }
+    else{
+        error[0].style.display="none"
+
+    }
+    let tip=(amount*tipValue)/100
+    
+    let totalBill=amount+tip
+
+    let billPerPerson=totalBill/people
+    // console.log(tip)
+    // console.log(totalBill)
+    // console.log(billPerPerson)
+    let tip_charged=document.getElementById('tip-charged')
+    tip_charged.innerText=tip
+    
+    let total_bill=document.getElementById('total-bill')
+    total_bill.innerText=totalBill
+
+    let Bill_per_person=document.getElementById('bill-per-person')
+    Bill_per_person.innerText=billPerPerson 
+}
+
